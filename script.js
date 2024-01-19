6705faf021f28dcdbbc2409130298a7d1d4a000d
Chafic Nehme <cnehm006@uottawa.ca>
2024-01-19 17:47:53 -0500

    out now on spotify

--- script.js
+++ script.js
@@ -4,22 +4,10 @@ document.addEventListener('DOMContentLoaded', function () {
         window.location.href = 'https://distrokid.com/hyperfollow/ch4fic/murd5scene';
     });
 
-    // Invisible button interaction
     const invisibleButton = document.getElementById('invisibleButton');
     invisibleButton.addEventListener('click', function() {
-        alert('Mira al amira, the miracle of my admiration <3');
+        alert("Mira my amira, the miracle of my admiration ❤️");
     });
 
-    // Example function to change song status
-    function updateSongStatus() {
-        const statusElement = document.getElementById('song-status');
-        statusElement.style.opacity = 0;
-        setTimeout(() => {
-            statusElement.textContent = 'Out Now on All Platforms';
-            statusElement.style.opacity = 1;
-        }, 500);
-    }
-
-    // Uncomment the following line when your song is released
-    // updateSongStatus();
+    // The song status update function is no longer needed here
 });
